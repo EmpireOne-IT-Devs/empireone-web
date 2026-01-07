a959e828a1206e492b7a634db556f864d0bb485f
Marlou Dev <web@empireonegroup.com>
2026-01-07 19:11:29 +0800

    revert adjustement

--- routes/api.php
+++ routes/api.php
@@ -11,5 +11,5 @@ Route::get('/user', function (Request $request) {
 
 
 
-Route::put('auth/login', [AuthorizationController::class, 'login']);
+Route::post('auth/login', [AuthorizationController::class, 'login']);
 Route::post('auth/google/app', [GoogleController::class, 'googleLogin']);
