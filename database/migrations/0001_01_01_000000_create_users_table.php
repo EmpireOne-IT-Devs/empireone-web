e2583f7c064e221767d21270be2f0861e4610248
QuicklyDG <quicklydeguzman@gmail.com>
2026-01-29 11:53:33 +0800

    optimize migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('gender')->nullable();
             $table->foreignId('app_id')
                 ->nullable()
                 ->constrained('personal_infos')
