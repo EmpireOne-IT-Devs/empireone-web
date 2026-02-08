a515f757371f4d84d69e56f4f97b2f8f0285c8ac
wakinnnnn_dev <eogs.whojilla@gmail.com>
2026-02-08 15:28:23 +0800

    added adjustment

--- resources/js/app/pages/administrator/job_requisition/page.jsx
+++ resources/js/app/pages/administrator/job_requisition/page.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import Layout from '../layout'
-import CardSection from './_sections/card-section'
-import HeaderSection from './_sections/header-section'
-import SearchSection from './_sections/search-section'
-import JobRequisitionCardSection from './_sections/job-requisition-card-section'
+import Layout from "../layout";
+import HeaderSection from "./_sections/header-section";
+import CardSection from "./_sections/card-section";
+import SearchSection from "./_sections/search-section";
+import JobRequisitionCardSection from "./_sections/job-requisition-card-section";
 
-export default function page() {
-  return (
-    <Layout>
-        <HeaderSection />
-       <CardSection />
-       <SearchSection />
-       <JobRequisitionCardSection />
-    </Layout>
-  )
+export default function Page() {
+    return (
+        <Layout>
+            <div className="space-y-6">
+                <HeaderSection />
+                <CardSection />
+                <SearchSection />
+                <JobRequisitionCardSection />
+            </div>
+        </Layout>
+    );
 }
