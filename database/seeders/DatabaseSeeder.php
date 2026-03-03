e7de43a624e11199b55ea3197999142ddd7edcb5
Marlou Dev <web@empireonegroup.com>
2026-03-04 02:34:10 +0800

    seeder adjustment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,9 +19,27 @@ class DatabaseSeeder extends Seeder
         User::create([
             'name' => 'Wakin Dev',
             'email' => 'eogs.whojilla@gmail.com',
+            'email_verified_at' => '2026-02-24 12:50:05',
             'password' => Hash::make('admin'),
             'role' => 1
         ]);
+
+        User::create([
+            'name' => 'Snickers Jay Magbanua',
+            'email' => 'snickersjay10@gmail.com',
+            'email_verified_at' => '2026-02-24 12:50:05',
+            'password' => Hash::make('admin'),
+            'role' => 1
+        ]);
+
+          User::create([
+            'name' => 'Marlou Dev',
+            'email' => 'webdev@empireonegroup.com',
+            'email_verified_at' => '2026-02-24 12:50:05',
+            'password' => Hash::make('admin'),
+            'role' => 1
+        ]);
+
 
         $this->call([
             DepartmentsTableSeeder::class,
