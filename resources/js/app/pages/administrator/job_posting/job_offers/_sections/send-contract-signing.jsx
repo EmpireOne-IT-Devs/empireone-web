690e31666e67442b33bdcb75cb26249147f77645
Marlou Dev <web@empireonegroup.com>
2026-03-27 13:52:24 +0800

    adjustment

--- resources/js/app/pages/administrator/job_posting/job_offers/_sections/send-contract-signing.jsx
+++ resources/js/app/pages/administrator/job_posting/job_offers/_sections/send-contract-signing.jsx
@@ -1,10 +1,22 @@
 import Button from '@/app/_components/button'
-import React from 'react'
+import Modal from '@/app/_components/modal'
+import React, { useState } from 'react'
 
 export default function SendContractSigning() {
+  const [open,setOpen]=useState(false)
   return (
     <div>
-        <Button variant='success'>SEND CONTRACT</Button>
+        <Button
+        onClick={()=>setOpen(true)}
+        variant='success'>SEND CONTRACT</Button>
+        <Modal
+        
+        
+                width="max-w-3xl"
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                title="Send Contact Signing"
+        ></Modal>
     </div>
   )
 }
