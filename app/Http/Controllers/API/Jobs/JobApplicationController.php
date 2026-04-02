661f010cb734f30e8d612f365e0a2d4314de532e
Marlou Dev <web@empireonegroup.com>
2026-04-02 19:37:00 +0800

    added more adjustment

--- app/Http/Controllers/API/Jobs/JobApplicationController.php
+++ app/Http/Controllers/API/Jobs/JobApplicationController.php
@@ -37,7 +37,7 @@ class JobApplicationController extends Controller
 
     public function get_job_application_by_user(Request $request)
     {
-        $applications =  JobApplication::where('user_id', Auth::id())->with(['applicant','job_posting'])->get();
+        $applications =  JobApplication::where('user_id', Auth::id())->with(['applicant', 'job_posting'])->get();
 
         return response()->json([
             'status' => 'success',
@@ -122,6 +122,7 @@ class JobApplicationController extends Controller
                 'birth_place' => $request->birth_place ?? null,
                 'nationality' => $request->nationality ?? null,
                 'marital_status' => $request->marital_status ?? null,
+                'contact' => $request->contact??null
             ]
         );
 
