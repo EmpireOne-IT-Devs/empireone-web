41aa80aec00d0c250c93cca22f257eca21d27a17
Marlou Dev <web@empireonegroup.com>
2026-07-07 14:26:35 +0800

    addiotnal ajustment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,7 +27,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <link rel="icon" type="image/png" href="/images/eo-full-logo.png">
+    <link rel="icon" type="image/png" href="/images/empireone.png.png">
 
     @routes
     @viteReactRefresh
