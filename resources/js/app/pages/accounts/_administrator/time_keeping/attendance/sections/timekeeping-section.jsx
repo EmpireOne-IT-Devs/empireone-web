b599181f0115c3f35af58f61df242d9b448beb63
QuicklyDG <quicklydeguzman@gmail.com>
2026-07-20 20:03:36 +0800

    adjust timekeeping UI

--- resources/js/app/pages/accounts/_administrator/time_keeping/attendance/sections/timekeeping-section.jsx
+++ resources/js/app/pages/accounts/_administrator/time_keeping/attendance/sections/timekeeping-section.jsx
@@ -12,6 +12,10 @@ export default function TimekeepingSection() {
 
         return () => clearInterval(timer);
     }, []);
+
+    const [selectedDate, setSelectedDate] = useState(
+        new Date().toISOString().split("T")[0],
+    );
     return (
         <div className="flex flex-1 gap-6">
             <div className="bg-white w-full rounded-2xl shadow-md p-6 border border-gray-200">
@@ -31,24 +35,41 @@ export default function TimekeepingSection() {
 
                 <div className="bg-gray-50 rounded-xl p-4 mb-6">
                     <div className="flex justify-between items-center">
+                        <div className="">
+                            <p className="text-sm text-gray-500">
+                                Current Time
+                            </p>
+                            <div>
+                                <p className="text-lg font-bold text-gray-600">
+                                    {currentTime.toLocaleDateString("en-US", {
+                                        weekday: "long",
+                                    })}
+                                </p>
+
+                                <p className="text-2xl font-bold text-blue-600">
+                                    {currentTime.toLocaleTimeString("en-US", {
+                                        hour: "2-digit",
+                                        minute: "2-digit",
+                                        second: "2-digit",
+                                        hour12: true,
+                                    })}
+                                </p>
+                            </div>
+                            <input
+                                type="date"
+                                value={selectedDate}
+                                onChange={(e) =>
+                                    setSelectedDate(e.target.value)
+                                }
+                                className="border rounded-lg px-2 py-2 mt-1 text-sm"
+                            />
+                        </div>
                         <div>
                             <p className="text-sm text-gray-500">
                                 Current Status
                             </p>
                             <p className="font-semibold text-green-600">
                                 Ready to Clock In
-                            </p>
-                        </div>
-
-                        <div className="text-right">
-                            <p className="text-sm text-gray-500">
-                                Current Time
-                            </p>
-                            <p className="text-lg font-bold text-blue-600">
-                                {currentTime.toLocaleTimeString()}
-                            </p>
-                            <p className="text-sm text-gray-600">
-                                {currentTime.toLocaleDateString()}
                             </p>
                         </div>
                     </div>
@@ -59,7 +80,14 @@ export default function TimekeepingSection() {
                         <LogIn size={20} />
                         Clock In
                     </button>
-
+                    <button className="flex items-center justify-center gap-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-xl transition-all shadow-sm">
+                        <LogOut size={20} />
+                        Break
+                    </button>
+                    <button className="flex items-center justify-center gap-2 bg-yellow-600 hover:bg-yellow-700 text-white font-semibold py-3 rounded-xl transition-all shadow-sm">
+                        <LogIn size={20} />
+                        Back from Break
+                    </button>
                     <button className="flex items-center justify-center gap-2 bg-red-600 hover:bg-red-700 text-white font-semibold py-3 rounded-xl transition-all shadow-sm">
                         <LogOut size={20} />
                         Clock Out
@@ -70,8 +98,23 @@ export default function TimekeepingSection() {
                     <h3 className="font-semibold text-gray-700 mb-3">
                         Today's Record
                     </h3>
-
-                    <div className="grid grid-cols-2 gap-4">
+                    <div className="grid grid-cols-3 gap-4">
+                        <div className="bg-green-50 p-3 rounded-lg">
+                            <p className="text-xs text-gray-500">
+                                Break Starts
+                            </p>
+                            <p className="font-bold text-green-700">--:--</p>
+                        </div>
+                        <div className="bg-red-50 p-3 rounded-lg">
+                            <p className="text-xs text-gray-500">Break Ends</p>
+                            <p className="font-bold text-red-700">--:--</p>
+                        </div>
+                        <div className="bg-orange-50 p-3 rounded-lg">
+                            <p className="text-xs text-gray-500">Durations</p>
+                            <p className="font-bold text-orange-700">--:--</p>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4 mt-5">
                         <div className="bg-green-50 p-3 rounded-lg">
                             <p className="text-xs text-gray-500">Clock In</p>
                             <p className="font-bold text-green-700">--:--</p>
