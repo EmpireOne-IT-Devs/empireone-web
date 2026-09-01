3102774bccef5334016e9aed3d053b16d8b6fc62
QuicklyDG <quicklydeguzman@gmail.com>
2026-09-01 21:10:33 +0800

    added attendance settings for payroll

--- resources/js/app/services/attendance-employee-settings-service.js
+++ resources/js/app/services/attendance-employee-settings-service.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 export async function create_attendance_employee_settings_service(data) {
-    return await axios.post("/api/attendance-employee-settings", data);
+    return await axios.post("/api/timekeeping/attendance_employee_settings", data);
 }
 
-export async function get_attendance_employee_settings_service() {
-    return await axios.get("/api/attendance-employee-settings");
+export async function get_attendance_employee_settings_service(employeeId) {
+    return await axios.get("/api/timekeeping/attendance_employee_settings", {
+        params: { employee_id: employeeId },
+    });
 }
